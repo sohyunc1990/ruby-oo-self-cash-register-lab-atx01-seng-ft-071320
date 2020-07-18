4d8a5ebd588d44ed3125ea6b49209a0266372d4f
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-18 23:15:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,77 +1,42 @@
 require 'pry'
-# class CashRegister
-# attr_accessor :discount, :total, :title, :price, :quantity, :item, :list_of_transaction
+ class CashRegister
+ attr_accessor :discount, :total, :title, :price, :quantity, :item, :list_of_transaction
   
-#   def initialize(discount = 0)
-#     @discount = discount
-#     @total = 0
-#     @cart = []
-#     @list_of_transaction = []
-#   end
-#   def total
-#     @total
-#   end
-#   def add_item(item, price, quantity= 1)
-#     @item = item
-#     @cart << item
-#     @price = price
-#     @quantity = quantity
-#     @total += (@price * quantity)
-#     @list_of_transaction << price
-#     until quantity == 1 do
-#       @list_of_transaction << price
-#         @cart << item
-#         quantity -=1
-#       end
-#   end
-#   def apply_discount
-#     if @discount > 0
-#     @discount = @discount/100.to_f
-#     @total = @total - (@total * (@discount))
-#     p "After the discount, the total comes to $#{@total.to_i}."
-#   else
-#     p "There is no discount to apply."
-#   end
-#   end
-#   def items
-#     @cart
-#   end
-#   def void_last_transaction
-#     @total -= @list_of_transaction.pop
-#   end
-# end
-
-class CashRegister
-  attr_accessor :discount, :total, :quantity
-  attr_reader :title, :price
-
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @cart = []
-  end
-  def add_item(title, price, quantity = 1)
-    @title = title
-    @price = price
-    @quantity = quantity
-    @cart << title
-    until quantity == 1 do
-    @cart << title
-    quantity -= 1
-    end
-    @total += (price * quantity)
-  end
-  def apply_discount
-    if discount > 0
-    @total -= (@total * discount)/100
-    "After the discount, the total comes to $#{@total}."  
-  else 
-    "There is no discount to apply."
-  end
-  end
-  def items
-    @cart
-    end
-    
-
-end
+   def initialize(discount = 0)
+     @discount = discount
+     @total = 0
+     @cart = []
+     @list_of_transaction = []
+   end
+  def total
+     @total
+   end
+   def add_item(item, price, quantity= 1)
+     @item = item
+     @cart << item
+     @price = price
+     @quantity = quantity
+     @total += (@price * quantity)
+     @list_of_transaction << price
+     until quantity == 1 do
+       @list_of_transaction << price
+         @cart << item
+         quantity -=1
+       end
+   end
+   def apply_discount
+     if @discount > 0
+     @discount = @discount/100.to_f
+     @total = @total - (@total * (@discount))
+     p "After the discount, the total comes to $#{@total.to_i}."
+   else
+     p "There is no discount to apply."
+   end
+   end
+   def items
+     @cart
+   end
+   def void_last_transaction
+     @total -= @list_of_transaction.pop
+   end
+ end
